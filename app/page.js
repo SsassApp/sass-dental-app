9af38904195d9915754f1c75be3b008849c6bfcb
sass-dental-app <sierrasass@sassdentalconsulting.com>
2026-04-08 10:36:03 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>SaaS Dental Owner Manager Reporting</h1>
+      <p>Your app is live 🚀</p>
+    </main>
+  );
+}
